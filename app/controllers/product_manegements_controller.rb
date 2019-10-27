22d49c12ee43f6b834c920fa0f43881acd0775f8
una7 <2ne1.jeremy@gmail.com>
2019-10-27 19:01:46 +0900

    update

--- app/controllers/product_manegements_controller.rb
+++ app/controllers/product_manegements_controller.rb
@@ -1,4 +1,12 @@
 class ProductManegementsController < ApplicationController
   def index 
   end
+
+  def age
+    # @ages = Age.where(Age: true)
+    # @ages = Age.all
+    # @ages = Age.find(params[:id])
+    # @ages = Age.find_by_age (age)
+    # モデル.find_by_カラム名 (検索する値)
+  end
 end
