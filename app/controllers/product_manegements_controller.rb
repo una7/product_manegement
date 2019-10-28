8afaf7089caf65906aa2dae3a1a54c7128c1241b
una7 <2ne1.jeremy@gmail.com>
2019-10-28 21:34:21 +0900

    Update product_manegements_controller.rb

--- app/controllers/product_manegements_controller.rb
+++ app/controllers/product_manegements_controller.rb
@@ -2,19 +2,29 @@ class ProductManegementsController < ApplicationController
   def index 
   end
 
-  # def product_manegement
-  #   # @product = Product.new 
-  # end
+  def new
+    @product_m = Product.new
+  end
+
+  def create
+    @product_m = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   def show
     @ages = Age.all
   end
 
   def age
+    @ages = Age.all
     # @ages = Age.where(Age: true)
     # @ages = Age.all
     # @ages = Age.find(params[:id])
     # @ages = Age.find(1)
-    @ages = Age.all
     # @ages = Age.where('age LIKE(?)',"% #{params[:keyword]}%").limit(5)
     # @ages = Age.find_by_age (age)
     # モデル.find_by_カラム名 (検索する値)
@@ -23,4 +33,5 @@ class ProductManegementsController < ApplicationController
   def search
     @ages = Age.where('age LIKE(?)',"% #{params[:keyword]}%").limit(5)
   end
+  
 end
