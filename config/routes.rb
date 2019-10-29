13054072847856d4e109d71604cd28b4edd33e74
una7 <2ne1.jeremy@gmail.com>
2019-10-29 17:30:07 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'product_manegements#index'
-  # get 'product_manegements/new'
   get 'product_manegements/sales'
   get 'product_manegements/age'
-  # get 'product_manegements/index' 
+  # get 'product_manegements/new'
   
   resources :product_manegements
 end
