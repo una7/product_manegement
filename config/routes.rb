67f8ed7cbf3f8a4ec699a1fb7668767edc8df5ef
una7 <2ne1.jeremy@gmail.com>
2019-11-07 18:28:23 +0900

    update page

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root 'product_manegements#index'
   
-  get 'product_manegements/age'
-  # get 'product_manegements/new'
+  resources :product_manegements, only:[:show,:new,:create,:destroy] do
+    collection do   
+      get 'registration'
+    end
+  end
   get 'sales/new'
   get 'sales/index'
+  get 'ages/index'
+  resources :ages
   
-  resources :product_manegements
 end
